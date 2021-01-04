22da4db9667b5f2dfb4173388f3f1ed586a13955
lirannl <liranpiade@gmail.com>
2021-01-05 03:35:09 +1000

    Added unicode support

--- converter.ts
+++ converter.ts
@@ -1,6 +1,6 @@
 const defaultCharSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/~!@#$%^&*;=?<>[]:\"{},`";
 
-const invalidCharSetError = new EvalError("Invalid character set. \
+const invalidCharSetError = () => new EvalError("Invalid character set. \
 Character sets must not contain the minus sign, or the fractional dot. \
 They must also not have duplicates.");
 
@@ -14,12 +14,12 @@ const hasLowerCaseAndUpperCase = (input: string) => (
 
 function isValidCharSet(charSet: string) {
     if (RegExp("[.-]").test(charSet)) return false;
-    if (RegExp("(.).*\\1").test(charSet)) return false;
+    if (RegExp("(.).*\\1", "u").test(charSet)) return false;
     return true;
 }
 
 export function baseParse(input: string, inputBase: number, inputSet = defaultCharSet): number {
-    if (!isValidCharSet(inputSet)) throw invalidCharSetError;
+    if (!isValidCharSet(inputSet)) throw invalidCharSetError();
     const trimmedInputSet = inputSet.slice(undefined, inputBase);
     const regexpParams = (() => {
         if (hasLowerCaseAndUpperCase(trimmedInputSet)) return undefined;
@@ -36,12 +36,12 @@ export function baseParse(input: string, inputBase: number, inputSet = defaultCh
         return index;
     });
     const lastPositionsIndex = positions.length - 1;
-    return positions.reduceRight(
+    return sign * positions.reduceRight(
         (total, curr, currIndex) => {
             const indexFromEnd = lastPositionsIndex - currIndex;
             return total + curr * (inputBase ** indexFromEnd);
         }
-        , 0) * sign;
+        , 0);
 }
 
 function recurBaseString(input: number, outputBase: number, outputSet: string, converted = ""): string {
@@ -52,7 +52,7 @@ function recurBaseString(input: number, outputBase: number, outputSet: string, c
 }
 
 export function baseString(input: number, outputBase = 10, outputSet = defaultCharSet): string {
-    if (!isValidCharSet(outputSet)) throw invalidCharSetError;
+    if (!isValidCharSet(outputSet)) throw invalidCharSetError();
     const sign = input < 0 ? "-" : "";
     return sign.concat(recurBaseString(Math.abs(input), outputBase, outputSet));
 }
